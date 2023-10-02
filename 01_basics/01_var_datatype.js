11c171ab6faa1d67e21a0c2e83ce3b50dc9e9464
soorajshetty604 <65722581+soorajshetty604@users.noreply.github.com>
2023-10-02 14:19:38 +0000

    Basics of variables and datatypes

---
+++ 01_basics/01_var_datatype.js
@@ -0,0 +1,10 @@
+const accountID = 387748
+let accountName = "sooraj shetty"
+
+var accountNominee = "Mahesh"  //do not prefer this.
+
+accountType = "savings"
+console.log(accountID)
+console.table([accountID, accountName, accountNominee])
+
+//int, bigInt, string, bool, object-->null, undefined
